bb9273331154655f513d6713bb0583c861969a2c
SamMurphy1 <samanthamurphy@wi.rr.com>
2016-02-06 16:55:34 -0600

    Updated AutoDrive

--- src/org/usfirst/frc/team930/robot/commands/AutoDrive.java
+++ src/org/usfirst/frc/team930/robot/commands/AutoDrive.java
@@ -1,15 +1,18 @@
 package org.usfirst.frc.team930.robot.commands;
 
+import org.usfirst.frc.team930.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class AutoDrive extends Command {
-
+	
     public AutoDrive() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.Drivetrain1);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +21,7 @@ public class AutoDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
