ab988c613e8473919ca38c695f77355c6c4c6b99
devin132 <milligandm@hotmail.com>
2016-02-11 19:43:22 -0600

    shooter

--- src/org/usfirst/frc/team930/robot/commands/ShootHighGoal.java
+++ src/org/usfirst/frc/team930/robot/commands/ShootHighGoal.java
@@ -35,7 +35,7 @@ public class ShootHighGoal extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
@@ -45,5 +45,6 @@ public class ShootHighGoal extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.shooter.setShooter(0);
     }
 }
