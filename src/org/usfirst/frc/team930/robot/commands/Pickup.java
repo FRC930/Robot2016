458cd150131428a7645221d4754220aa1a725054
paulrinaldi <paulrinaldi10@yahoo.com>
2016-02-19 18:25:25 -0600

    Added some lines in Pickup Command

--- src/org/usfirst/frc/team930/robot/commands/Pickup.java
+++ src/org/usfirst/frc/team930/robot/commands/Pickup.java
@@ -1,15 +1,24 @@
 package org.usfirst.frc.team930.robot.commands;
 
+import org.usfirst.frc.team930.robot.subsystems.IntakeLifter;
+import org.usfirst.frc.team930.robot.subsystems.IntakeRoller;
+
 import edu.wpi.first.wpilibj.command.Command;
 
+import org.usfirst.frc.team930.robot.Robot;
 /**
  *
  */
 public class Pickup extends Command {
 
+	
+	
     public Pickup() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	
+    	requires(Robot.intakeRoller);
+    	requires(Robot.intakeLifter);
     }
 
     // Called just before this Command runs the first time
@@ -18,19 +27,25 @@ public class Pickup extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.intakeLifter.setAngle(IntakeLifter.Position.LOW);
+    	Robot.intakeRoller.setState(IntakeRoller.Direction.FORWARD);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.intakeRoller.seeBall();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.intakeLifter.setAngle(IntakeLifter.Position.HIGH);
+    	Robot.intakeRoller.setState(IntakeRoller.Direction.STOP);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.intakeLifter.setAngle(IntakeLifter.Position.HIGH);
+    	Robot.intakeRoller.setState(IntakeRoller.Direction.STOP);
     }
 }
