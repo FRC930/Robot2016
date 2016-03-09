29088c4475ed32bf068ae3b49ec9df493b03ce0a
devin132 <milligandm@hotmail.com>
2016-03-08 20:38:03 -0600

    auto shoot under low goal

--- src/org/usfirst/frc/team930/robot/commands/AutoLowBarShoot.java
+++ src/org/usfirst/frc/team930/robot/commands/AutoLowBarShoot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team930.robot.commands;
 
 import org.usfirst.frc.team930.robot.Robot;
+import org.usfirst.frc.team930.robot.subsystems.IntakeLifter;
 import org.usfirst.frc.team930.robot.subsystems.IntakeRoller;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -15,9 +16,11 @@ public class AutoLowBarShoot extends Command {
 	
 	final int START = 0;
 	final int DRIVE = 1;
-	final int SHOOT = 2;
-	final int TURNING_OFF = 3;
-	final int END = 4;
+	final int TURN = 2;
+	final int DRIVE_2 = 3;
+	final int SHOOT = 4;
+	final int TURNING_OFF = 5;
+	final int END = 6;
 	int state = 0;
 	
 	double startTime; // time the command starts running (seconds)
@@ -37,6 +40,7 @@ public class AutoLowBarShoot extends Command {
     	Robot.drivetrain.drivePID.setSetpoint(0.0);
     	state = START;
     	timer.start();
+    	startTime = timer.get();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -44,14 +48,54 @@ public class AutoLowBarShoot extends Command {
     	
     	Robot.drivetrain.drivePID.enable();
     	if(state == START){
-    		startTime = timer.get(); // gets the starting time in seconds
+    		currentTime = timer.get(); // gets the current time
+    		Robot.intakeLifter.setAngle(IntakeLifter.Positions.PICKUP);
     		state = DRIVE;
     	}
-    	
     	if(state == DRIVE){
     		Robot.drivetrain.setL(0.25);
     		Robot.drivetrain.setL(0.25);
-
+    		currentTime = timer.get();
+    		if(currentTime - startTime >6){
+    			state = TURN;
+    		}
+    	}
+    	if(state == TURN){
+    	Robot.drivetrain.drivePID.setSetpoint(60.0);
+    	currentTime = timer.get();
+    	if(currentTime-startTime > 6.5){
+    		state = DRIVE_2;
+    	}
+    	}
+    	if(state == DRIVE_2){
+    		Robot.drivetrain.setL(0.6);
+    		Robot.drivetrain.setR(0.6);
+    		currentTime = timer.get();
+    		if(currentTime - startTime >8.5){
+    			Robot.drivetrain.setL(0);
+        		Robot.drivetrain.setR(0);
+    			state = SHOOT;	
+    		}
+    	}
+    	if(state == SHOOT){
+    		Robot.shooter.setShooter(0.5 * 6000);
+    		currentTime = timer.get();
+    		if(currentTime - startTime > 8.7){
+    			Robot.shooter.setShooter(0.75 * 6000);
+    		}
+    		currentTime = timer.get();
+    		if(currentTime - startTime> 9.0){
+    			Robot.shooter.setShooter(6000);
+    		}
+    		currentTime = timer.get();
+    		if(currentTime - startTime > 12.0){
+    			Robot.intakeRoller.setState(IntakeRoller.Direction.SHOOTERPULL);
+    		}
+    	}
+    	state = TURNING_OFF;
+    	if(state == TURNING_OFF){
+    		Robot.intakeRoller.setState(IntakeRoller.Direction.STOP);
+    		state = END;
     	}
     }
     // Make this return true when this Command no longer needs to run execute()
